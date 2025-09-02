5a2f43127f03008a49db630c2b8925757075ccce
flapdzak <parowka18888@gmail.com>
2025-09-02 10:57:22 +0200

    Added base for possess power

---
+++ lib/GameScrens/03_Haunting/Haunting_Game/Classes/Ghost/Subclasses/Power/Mechanics/UsingPowers/ByType/Possessing/PowersPossess.dart
@@ -0,0 +1,11 @@
+import 'package:happyhaunting/GameScrens/03_Haunting/Haunting_Game/Classes/Ghost/Haunting_Ghost.dart';
+import 'package:happyhaunting/GameScrens/03_Haunting/Haunting_Game/Classes/Ghost/Subclasses/Power/Haunting_Power.dart';
+import 'package:happyhaunting/GameScrens/03_Haunting/Haunting_Game/Classes/Room/Haunting_Room.dart';
+import 'package:happyhaunting/GameScrens/03_Haunting/Haunting_Game/Haunting_Game.dart';
+
+class PowersPossess{
+  static void usePower_PossessMortal(Haunting_Power power, Haunting_Ghost ghost, Haunting_Room room, Haunting_Game game) {
+
+  }
+
+}
