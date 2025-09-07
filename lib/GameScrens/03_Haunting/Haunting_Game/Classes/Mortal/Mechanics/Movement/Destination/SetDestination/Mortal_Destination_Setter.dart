29e9d800d743629c967046f2ac56337a29cad9fe
flapdzak <parowka18888@gmail.com>
2025-09-07 13:11:05 +0200

    Fixed special action points for floor

--- lib/GameScrens/03_Haunting/Haunting_Game/Classes/Mortal/Mechanics/Movement/Destination/SetDestination/Mortal_Destination_Setter.dart
+++ lib/GameScrens/03_Haunting/Haunting_Game/Classes/Mortal/Mechanics/Movement/Destination/SetDestination/Mortal_Destination_Setter.dart
@@ -52,7 +52,7 @@ class Mortal_Destination_Setter{
         mortal.path = path.map((p) => gridToWorld(p)).toList();
       }
       else {
-        Mortal_Destination_Setter.clearDestinationPoints(mortal, game);
+        // Mortal_Destination_Setter.clearDestinationPoints(mortal, game);
         // Mortal_Destination_Navigator.setMortalNextDestination_Navigator(mortal, game);
       }
 
