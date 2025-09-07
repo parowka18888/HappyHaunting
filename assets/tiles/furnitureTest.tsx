2690872426c456d8dfcdf5e71516e9d56e978970
flapdzak <parowka18888@gmail.com>
2025-09-07 11:20:01 +0200

    Updated side panel

---
+++ assets/tiles/furnitureTest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="furnitureTest" tilewidth="16" tileheight="16" tilecount="6144" columns="12">
+ <image source="../images/Levels/Test/furnitureTest.png" trans="ff00ff" width="206" height="137"/>
+</tileset>
